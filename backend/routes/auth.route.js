843371cf9e30d51da0a416f5efe0ec500fdd3c82
Sukul <sukulsarve123@gmail.com>
2024-05-23 01:28:01 +0530

    user routes and controller

--- backend/routes/auth.route.js
+++ backend/routes/auth.route.js
@@ -1,6 +1,6 @@
 import express from "express";
-import {signup,login,logout, getme} from "../controllers/auth.controller.js"
 import { protectRoute } from "../moddleware/protectRoute.js";
+import {signup,login,logout, getme} from "../controllers/auth.controller.js"
 
 const router = express.Router();
 
--- backend/server.js
+++ backend/server.js
@@ -1,11 +1,21 @@
 import express from "express";
-import authRoute from './routes/auth.route.js'  // we have to write .js because in package.json we wrote type as module
-
 import dotenv from 'dotenv';
-import connectMongodb from "./db/connectMongodb.js";
 import cookieParser from "cookie-parser";
+import{v2 as cloudinary} from "cloudinary";
+
+import authRoute from './routes/auth.route.js'  // we have to write .js because in package.json we wrote type as module
+import userRoute from './routes/user.route.js'
+
+import connectMongodb from "./db/connectMongodb.js";
 
 dotenv.config();
+
+cloudinary.config({
+    cloud_name:process.env.CLOUDINARY_CLOUD_NAME,
+    api_key:process.env.CLOUDINARY_API_KEY,
+    api_secret:process.env.CLOUDINARY_API_SECRET,
+});
+
 const app = express();
 const PORT = process.env.PORT
 
@@ -17,6 +27,7 @@ app.use(express.urlencoded({extended:true}));
 app.use(cookieParser());  // to get the cookie in differnt function like in the protectRoute
 
 app.use("/api/auth",authRoute);
+app.use("/api/users",userRoute);
 
 
 
