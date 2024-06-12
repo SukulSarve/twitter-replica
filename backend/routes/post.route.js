b8f19e3117247db9fb9ed1b8c0a6e745f77821b2
Sukul <sukulsarve123@gmail.com>
2024-06-12 16:21:39 +0530

    Post route and controller added

---
+++ backend/routes/post.route.js
@@ -0,0 +1,27 @@
+import express from "express";
+
+import { protectRoute } from "../moddleware/protectRoute.js";
+
+import { createPost } from "../controllers/post.controllers.js";
+import { deletePost } from "../controllers/post.controllers.js";
+import { commentOnPost } from "../controllers/post.controllers.js";
+import { likeUnlikePost } from "../controllers/post.controllers.js";
+import { getAllPost } from "../controllers/post.controllers.js";
+import { getLikedPosts } from "../controllers/post.controllers.js";
+import { getFollowingPosts } from "../controllers/post.controllers.js";
+import { getUserPosts } from "../controllers/post.controllers.js";
+
+const router = express.Router();
+
+router.get("/all",protectRoute,getAllPost)
+router.get("/following",protectRoute,getFollowingPosts)
+router.get("/likes/:id",protectRoute,getLikedPosts)
+router.get("/user/:username",protectRoute,getUserPosts)
+router.post("/create",protectRoute,createPost)
+router.post("/like/:id",protectRoute,likeUnlikePost)
+router.post("/comment/:id",protectRoute,commentOnPost)
+router.delete("/:id",protectRoute,deletePost)
+
+ 
+
+export default router;
--- backend/server.js
+++ backend/server.js
@@ -5,6 +5,7 @@ import{v2 as cloudinary} from "cloudinary";
 
 import authRoute from './routes/auth.route.js'  // we have to write .js because in package.json we wrote type as module
 import userRoute from './routes/user.route.js'
+import postRoute from './routes/post.route.js'
 
 import connectMongodb from "./db/connectMongodb.js";
 
@@ -28,6 +29,7 @@ app.use(cookieParser());  // to get the cookie in differnt function like in the 
 
 app.use("/api/auth",authRoute);
 app.use("/api/users",userRoute);
+app.use("/api/posts",postRoute);
 
 
 
